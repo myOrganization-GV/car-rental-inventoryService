de5e384f6a7fe6d8ce65c8d7d29bc714813cea6d
Guilherme Vale <guilherme.vale98@gmail.com>
2025-03-01 14:51:37 -0300

    start inventory service

---
+++ src/main/java/com/gui/car_rental_inventoryService/CarRentalInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_inventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gui/car_rental_inventoryService/CarRentalInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.car_rental_inventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
